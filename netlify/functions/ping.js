06cedda09c3febce681e9f5da14bd37113e19135
bechobazaar <laxystudiocreations@gmail.com>
2025-10-09 08:59:58 +0530

    Create ping.js

---
+++ netlify/functions/ping.js
@@ -0,0 +1,15 @@
+const cors = () => ({
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+});
+exports.handler = async (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return { statusCode: 204, headers: cors(), body: "" };
+  }
+  return {
+    statusCode: 200,
+    headers: cors(),
+    body: JSON.stringify({ ok: true, time: new Date().toISOString() }),
+  };
+};
